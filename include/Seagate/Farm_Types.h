cadd2d1327575ea55c64f23ef5cfe03dc7034a01
Tim Gilmer <Tim.Gilmer@seagate.com>
2019-01-30 20:31:53 -0700

    moving the functions to a new file farm_helper.h and moving the sturcts back in out of the class, for portability.

--- include/Seagate/Farm_Types.h
+++ include/Seagate/Farm_Types.h
@@ -16,196 +16,47 @@
 
 #include <inttypes.h>
 #include "Opensea_Parser_Helper.h"
+#include "Farm_Helper.h"
 
+#define MAX_HEAD_COUNT			24
 #define FARMSIGNATURE           0x00004641524D4552
-#define MAX_HEAD_COUNT 24
+
 #pragma pack(push, 1)
 
-//-----------------------------------------------------------------------------
-//
-//! \fn check_For_Active_Status()
-//
-//! \brief
-//!   Description:  check for the active status bit in the 64 bit value
-//
-//  Entry:
-//! \param value  =  64 bit value to check to see if the bit is set or not
-//
-//  Exit:
-//!   \return bool - false or true
-//
-//---------------------------------------------------------------------------
-inline bool check_For_Active_Status(uint64_t *value)
+typedef struct _sStringIdentifyData
 {
-    if ((*value & BIT63) == BIT63 && (*value & BIT62) == BIT62)
-    {
-        return true;
-    }
-    return false;
-}
-//-----------------------------------------------------------------------------
-//
-//! \fn check_Status_Strip_Status()
-//
-//! \brief
-//!   Description:  check for the active status bit in the 64 bit value
-//
-//  Entry:
-//! \param value  =  64 bit value to check to see if the bit is set or not
-//
-//  Exit:
-//!   \return uint64_t return the stipped value or a 0
-//
-//---------------------------------------------------------------------------
-inline uint64_t check_Status_Strip_Status(uint64_t value)
+	std::string serialNumber;										//!< seiral number of the drive
+	std::string worldWideName;										//!< World Wide Name of the device
+	std::string deviceInterface;									//!< Device Interface 
+	std::string firmwareRev;										//!< Firmware Rev
+}sStringIdentifyData;
+
+typedef struct _sFarmHeader
 {
-	if (check_For_Active_Status(&value))
-	{
-		value = value & 0x00FFFFFFFFFFFFFFLL;
-	}
-	else
-	{
-		value = 0;
-	}
-	return value;
-}
+	uint64_t        signature;                                  //!< Log Signature = 0x00004641524D4552
+	uint64_t        majorRev;                                   //!< Log Major rev
+	uint64_t        minorRev;                                   //!< minor rev 
+	uint64_t        pagesSupported;                             //!< number of pages supported
+	uint64_t        logSize;                                    //!< log size in bytes
+	uint64_t        pageSize;                                   //!< page size in bytes
+	uint64_t        headsSupported;                             //!< Maximum Drive Heads Supported
+	uint64_t        copies;                                     //!< Number of Historical Copies
+}sFarmHeader;
 
-inline void set_json_64_bit_With_Status(JSONNODE *nowNode, const std::string & myStr, int64_t value, bool hexPrint, bool showStatusBits)
+typedef struct _sWorkLoadStat
 {
-	std::string printStr = " ";
-	printStr.resize(BASIC);
-	std::string lowStr = "64 bit Value Lower value";
-	std::string upperStr = "64 bit Value Upper value";
+	uint64_t        pageNumber;                                 //!< Page Number = 2
+	uint64_t        copyNumber;                                 //!< Copy Number
+	uint64_t        workloadPercentage;                         //!< rated Workload Percentage
+	uint64_t        totalReadCommands;                          //!< Total Number of Read Commands
+	uint64_t        totalWriteCommands;                         //!< Total Number of Write Commands
+	uint64_t        totalRandomReads;                           //!< Total Number of Random Read Commands
+	uint64_t        totalRandomWrites;                          //!< Total Number of Random Write Commands
+	uint64_t        totalNumberofOtherCMDS;                     //!< Total Number Of Other Commands
+	uint64_t        logicalSecWritten;                          //!< Logical Sectors Written
+	uint64_t        logicalSecRead;                             //!< Logical Sectors Read
+}sWorkLoadStat;
 
 
-	JSONNODE *bigBit = json_new(JSON_NODE);
-	json_set_name(bigBit, (char *)myStr.c_str());
-	if (showStatusBits)
-	{
-		if ((value & BIT63) == BIT63)
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Supported", true);
-		}
-		else
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Supported", false);
-		}
-		if ((value & BIT62) == BIT62)
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Valid", true);
-		}
-		else
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Valid", false);
-		}
-	}
-	value = check_Status_Strip_Status(value);
-	value = check_for_signed_int(value,16);
-	int32_t lowValue = static_cast<int32_t>(value);
-	int32_t upperValue = static_cast<int32_t>(value >> 32);
-	if (hexPrint)
-	{
-		if (value > INT32_MAX)
-		{
-			json_push_back(bigBit, json_new_b("64 bit Value String in Hex", true));
-			snprintf((char*)printStr.c_str(), BASIC, "0x%014" PRIx64"", value);
-			json_push_back(bigBit, json_new_a("64 bit Value String", (char*)printStr.c_str()));
-			json_push_back(bigBit, json_new_i((char*)lowStr.c_str(), lowValue));
-			json_push_back(bigBit, json_new_i((char*)upperStr.c_str(), upperValue));
-			json_push_back(nowNode, bigBit);
-		}
-		else
-		{
-			snprintf((char*)printStr.c_str(), BASIC, "0x%08" PRIx32"", lowValue);
-			json_push_back(nowNode, json_new_a((char *)myStr.c_str(), (char*)printStr.c_str()));
-		}
-	}
-	else
-	{
-		if (value > INT32_MAX)
-		{
-			json_push_back(bigBit, json_new_b("64 bit Value String in Hex", false));
-			snprintf((char*)printStr.c_str(), BASIC, "%" PRIi64"", value);
-			json_push_back(bigBit, json_new_a("64 bit Value String", (char*)printStr.c_str()));
-			json_push_back(bigBit, json_new_i((char*)lowStr.c_str(), lowValue));
-			json_push_back(bigBit, json_new_i((char*)upperStr.c_str(), upperValue));
-			json_push_back(nowNode, bigBit);
-		}
-		else
-		{
-			json_push_back(nowNode, json_new_i((char *)myStr.c_str(), static_cast<int32_t>(M_DoubleWord0(value))));
-		}
-	}
-	
-}
-
-inline void set_json_int_With_Status(JSONNODE *nowNode, const std::string & myStr, int64_t value, bool showStatusBits)
-{
-	std::string printStr = " ";
-	printStr.resize(BASIC);
 
 
-	if (showStatusBits)
-	{
-		JSONNODE *bigBit = json_new(JSON_NODE);
-		json_set_name(bigBit, (char *)myStr.c_str());
-		if ((value & BIT63) == BIT63)
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Supported", true);
-		}
-		else
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Supported", false);
-		}
-		if ((value & BIT62) == BIT62)
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Valid", true);
-		}
-		else
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Valid", false);
-		}
-		value = check_Status_Strip_Status(value);
-		json_push_back(bigBit, json_new_i((char *)myStr.c_str(), static_cast<int32_t>(M_DoubleWord0(value))));
-		json_push_back(nowNode, bigBit);
-	}
-	else
-	{
-		value = check_Status_Strip_Status(value);
-		json_push_back(nowNode, json_new_i((char *)myStr.c_str(), static_cast<int32_t>(M_DoubleWord0(value))));
-	}
-}
-
-inline void set_json_string_With_Status(JSONNODE *nowNode, const std::string & myStr, const std::string & strValue, uint64_t value, bool showStatusBits)
-{
-	std::string printStr = " ";
-	printStr.resize(BASIC);
-
-	if (showStatusBits)
-	{
-		JSONNODE *bigBit = json_new(JSON_NODE);
-		json_set_name(bigBit, (char *)myStr.c_str());
-		if ((value & BIT63) == BIT63)
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Supported", true);
-		}
-		else
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Supported", false);
-		}
-		if ((value & BIT62) == BIT62)
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Valid", true);
-		}
-		else
-		{
-			opensea_parser::set_Json_Bool(bigBit, "Field Valid", false);
-		}
-		json_push_back(bigBit, json_new_a((char *)myStr.c_str(), (char *)strValue.c_str()));
-		json_push_back(nowNode, bigBit);
-	}
-	else
-	{
-		json_push_back(nowNode, json_new_a((char *)myStr.c_str(), (char *)strValue.c_str()));
-	}
-}
