220ef5e6b355287007bf0b8685696bdbb11d2b3e
Tim <Timothy.O.Gilmer@seagate.com>
2019-05-21 10:16:42 -0600

    moving support functions to opensea_parser_helper for other logs

--- include/Seagate/Farm_Helper.h
+++ include/Seagate/Farm_Helper.h
@@ -20,34 +20,71 @@
 namespace opensea_parser {
 #ifndef FARMHELPER
 #define FARMHELPER
+
 	//-----------------------------------------------------------------------------
 	//
-	//! \fn check_for_signed_int()
+	//! \fn set_json_64()
 	//
 	//! \brief
-	//!   Description:  check for the active status bit in the 64 bit value, then stips the status off and checks the 
-	//!		bits to see if it is a signed int and saves the value
+	//!   Description:  set the json values for a 64 bit value
 	//
 	//  Entry:
+	//! \param  nowNode = the Json node that the data will be added to
+	//! \param  myStr = the string data what will be adding to
 	//! \param value  =  64 bit value to check to see if the bit is set or not
-	//! \param length = the amount we need to move to the left and the right to remove the status 
+	//! \param hexPrint =  if true then print the data in a hex format
 	//
 	//  Exit:
-	//!   \return int64_t return the signed stipped value or a 0
+	//!   \return void
 	//
-	//---------------------------------------------------------------------------
-	inline int64_t check_for_signed_int(int64_t value, uint32_t length)
+	//-----------------------------------------------------------------------------
+	/*
+	inline void set_json_64_Only(JSONNODE *nowNode, const std::string & myStr, int64_t value, bool hexPrint)
 	{
-		int8_t neg = -1;			// set to 1 if bit 56 is set then set it to -1
-		value = value << length;
-		if ((value & BIT63) == BIT63)   // check to see if the bit is set for nef number
+		std::string printStr = " ";
+		printStr.resize(BASIC);
+		std::string lowStr = "64 bit Value Lower value";
+		std::string upperStr = "64 bit Value Upper value";
+
+		value = check_Status_Strip_Status(value);
+		value = check_for_signed_int(value, 16);
+		int32_t lowValue = static_cast<int32_t>(value);
+		int32_t upperValue = static_cast<int32_t>(value >> 32);
+		if (hexPrint)
 		{
-			value = M_2sCOMPLEMENT(value);
-			value = value * neg;
+#ifndef _WIN64   //To make old gcc compilers happy
+			JSONNODE *bigBit = json_new(JSON_NODE);
+			json_set_name(bigBit, (char *)myStr.c_str());
+			json_push_back(bigBit, json_new_b("64 bit Value String in Hex", true));
+			snprintf((char*)printStr.c_str(), BASIC, "0x%014" PRIx64"", value);
+			json_push_back(bigBit, json_new_a("64 bit Value String", (char*)printStr.c_str()));
+			json_push_back(bigBit, json_new_i((char*)lowStr.c_str(), lowValue));
+			json_push_back(bigBit, json_new_i((char*)upperStr.c_str(), upperValue));
+			json_push_back(nowNode, bigBit);
+#else		//json does not support 64 bit numbers. Therefore we will print it as a string
+			snprintf((char*)printStr.c_str(), BASIC, "0x%08" PRIx64"", value);
+			json_push_back(nowNode, json_new_a((char *)myStr.c_str(), (char*)printStr.c_str()));
+#endif
 		}
-		value = value >> length;      // move the back to the original number
-		return(value);
+		else
+		{
+#ifndef _WIN64   //To make old gcc compilers happy
+			JSONNODE *bigBit = json_new(JSON_NODE);
+			json_set_name(bigBit, (char *)myStr.c_str());
+			json_push_back(bigBit, json_new_b("64 bit Value String in Hex", false));
+			snprintf((char*)printStr.c_str(), BASIC, "%" PRIi64"", value);
+			json_push_back(bigBit, json_new_a("64 bit Value String", (char*)printStr.c_str()));
+			json_push_back(bigBit, json_new_i((char*)lowStr.c_str(), lowValue));
+			json_push_back(bigBit, json_new_i((char*)upperStr.c_str(), upperValue));
+			json_push_back(nowNode, bigBit);
+#else		//json does not support 64 bit numbers. Therefore we will print it as a string
+			snprintf((char*)printStr.c_str(), BASIC, "%" PRId64"", value);
+			json_push_back(nowNode, json_new_a((char *)myStr.c_str(), (char*)printStr.c_str()));
+#endif
+		}
+
 	}
+	*/
 	//-----------------------------------------------------------------------------
 	//
 	//! \fn set_json_64_bit_With_Status()
@@ -72,8 +109,9 @@ namespace opensea_parser {
 		printStr.resize(BASIC);
 		std::string lowStr = "64 bit Value Lower value";
 		std::string upperStr = "64 bit Value Upper value";
+		uint64_t strippedValue = check_Status_Strip_Status(value);
 		
-		if (!showStatusBits && (value < INT32_MAX)) 
+		if (!showStatusBits && (strippedValue < INT32_MAX && strippedValue > INT32_MIN))
 		{
 			if (hexPrint) 
 			{
@@ -93,60 +131,29 @@ namespace opensea_parser {
 		{
 			if ((value & BIT63) == BIT63)
 			{
-				opensea_parser::set_Json_Bool(bigBit, "Field Supported", true);
+				set_Json_Bool(bigBit, "Field Supported", true);
 			}
 			else
 			{
-				opensea_parser::set_Json_Bool(bigBit, "Field Supported", false);
+				set_Json_Bool(bigBit, "Field Supported", false);
 			}
 			if ((value & BIT62) == BIT62)
 			{
-				opensea_parser::set_Json_Bool(bigBit, "Field Valid", true);
+				set_Json_Bool(bigBit, "Field Valid", true);
 			}
 			else
 			{
-				opensea_parser::set_Json_Bool(bigBit, "Field Valid", false);
+				set_Json_Bool(bigBit, "Field Valid", false);
 			}
-		}
-		value = check_Status_Strip_Status(value);
-		value = check_for_signed_int(value, 16);
-		int32_t lowValue = static_cast<int32_t>(value);
-		int32_t upperValue = static_cast<int32_t>(value >> 32);
-		if (hexPrint)
-		{
-			if (value > INT32_MAX)
-			{
-				json_push_back(bigBit, json_new_b("64 bit Value String in Hex", true));
-				snprintf((char*)printStr.c_str(), BASIC, "0x%014" PRIx64"", value);
-				json_push_back(bigBit, json_new_a("64 bit Value String", (char*)printStr.c_str()));
-				json_push_back(bigBit, json_new_i((char*)lowStr.c_str(), lowValue));
-				json_push_back(bigBit, json_new_i((char*)upperStr.c_str(), upperValue));
-				json_push_back(nowNode, bigBit);
-			}
-			else
-			{
-				snprintf((char*)printStr.c_str(), BASIC, "0x%08" PRIx32"", lowValue);
-				json_push_back(nowNode, json_new_a((char *)myStr.c_str(), (char*)printStr.c_str()));
-			}
+			set_json_64bit(bigBit, myStr, value, hexPrint);
+			json_push_back(nowNode, bigBit);
 		}
 		else
 		{
-			if (value > INT32_MAX)
-			{
-				json_push_back(bigBit, json_new_b("64 bit Value String in Hex", false));
-				snprintf((char*)printStr.c_str(), BASIC, "%" PRIi64"", value);
-				json_push_back(bigBit, json_new_a("64 bit Value String", (char*)printStr.c_str()));
-				json_push_back(bigBit, json_new_i((char*)lowStr.c_str(), lowValue));
-				json_push_back(bigBit, json_new_i((char*)upperStr.c_str(), upperValue));
-
-			}
-			else
-			{
-				json_push_back(bigBit, json_new_i((char *)myStr.c_str(), static_cast<int32_t>(M_DoubleWord0(value))));
-			}
+			set_json_64bit(nowNode, myStr, value, hexPrint);
 		}
-		json_push_back(nowNode, bigBit);
 	}
+
 	//-----------------------------------------------------------------------------
 	//
 	//! \fn set_json_int_With_Status()
