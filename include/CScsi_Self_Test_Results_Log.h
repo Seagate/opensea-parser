1da42237670b95993c3174f076924754af84bf66
Tim <Timothy.O.Gilmer@seagate.com>
2021-04-28 17:13:11 -0600

    getting DST to work

--- include/CScsi_Self_Test_Results_Log.h
+++ include/CScsi_Self_Test_Results_Log.h
@@ -76,16 +76,25 @@ namespace opensea_parser {
 		uint8_t						m_SubPage;					//<! passed in- this is subpage that tell us what log to parse as.
         sSelfTest					*m_DST;						//<! single dst results
 
-		eReturnValues parse_Self_Test_Log(uint8_t * buffer, size_t bufferSize, JSONNODE *masterJson);
+		eReturnValues get_Self_Test_Log(uint8_t * buffer, size_t bufferSize, JSONNODE *masterJson);
+		eReturnValues get_PrePython_Self_Test_Log(uint8_t* buffer, size_t bufferSize, JSONNODE* masterJson);
 		void print_Self_Test_Log(JSONNODE *dstNode, uint16_t run);
 		void get_Self_Test_Results_String(std::string & meaning, uint8_t result);
+		void get_PrePython_Self_Test_Results_String(std::string& meaning, uint8_t result);
 		void byte_Swap_Self_Test();
 	public:
 		CScsi_DST_Results();
 		CScsi_DST_Results(uint8_t *bufferData, size_t bufferSize, JSONNODE *masterJson);
 		virtual ~CScsi_DST_Results();
 		virtual eReturnValues get_Log_Status() { return m_DSTStatus; };
-
+		virtual eReturnValues parse_Self_Test_Log(uint8_t* buffer, size_t bufferSize, JSONNODE* masterJson)
+		{
+#if defined (PREPYTHON)
+			return get_PrePython_Self_Test_Log(buffer, bufferSize, masterJson);
+#else
+			return get_Self_Test_Log(buffer, bufferSize, masterJson);
+#endif
+		};
 	};
 #endif
 }
--- src/CScsi_Self_Test_Results_Log.cpp
+++ src/CScsi_Self_Test_Results_Log.cpp
@@ -97,7 +97,7 @@ CScsi_DST_Results::~CScsi_DST_Results()
 }
 //-----------------------------------------------------------------------------
 //
-//! \fn parse_Self_Test_Log()
+//! \fn get_Self_Test_Log()
 //
 //! \brief
 //!   Description: parse the ext self test dst log 
@@ -110,7 +110,7 @@ CScsi_DST_Results::~CScsi_DST_Results()
 //!   \return eReturnValues success
 //
 //---------------------------------------------------------------------------
-eReturnValues CScsi_DST_Results::parse_Self_Test_Log(uint8_t *buffer, size_t bufferSize, JSONNODE *masterJson)
+eReturnValues CScsi_DST_Results::get_Self_Test_Log(uint8_t *buffer, size_t bufferSize, JSONNODE *masterJson)
 {
 	eReturnValues retStatus = IN_PROGRESS;
 	std::string myStr = "";
@@ -159,6 +159,60 @@ eReturnValues CScsi_DST_Results::parse_Self_Test_Log(uint8_t *buffer, size_t buf
 }
 //-----------------------------------------------------------------------------
 //
+//! \fn get_PrePython_Self_Test_Log()
+//
+//! \brief
+//!   Description: parse the ext self test dst log 
+//
+//  Entry:
+//! \param buffer -   pointer to the buffer data
+//! \param masterData - json node for all the data
+//
+//  Exit:
+//!   \return eReturnValues success
+//
+//---------------------------------------------------------------------------
+eReturnValues CScsi_DST_Results::get_PrePython_Self_Test_Log(uint8_t* buffer, size_t bufferSize, JSONNODE* masterJson)
+{
+	eReturnValues retStatus = IN_PROGRESS;
+	std::string myStr = "";
+	myStr.resize(BASIC);
+
+	m_DST = (sSelfTest*)buffer;
+	
+	for (int i = 1; i <= 20; i++)
+	{
+		byte_Swap_Self_Test();
+		if (m_DST->paramCode == i && m_DST->paramLength == 0x10)
+		{
+			print_Self_Test_Log(masterJson, i);
+			if (i == 20)
+			{
+				retStatus = SUCCESS;
+			}
+			else
+			{
+				if ((((size_t)i + 1) * sizeof(sSelfTest)) <= bufferSize)
+				{
+					m_DST++;
+					retStatus = IN_PROGRESS;
+				}
+				else
+				{
+					return BAD_PARAMETER;
+				}
+			}
+		}
+		else
+		{
+			retStatus = BAD_PARAMETER;
+			break;
+		}
+	}
+	return retStatus;
+}
+//-----------------------------------------------------------------------------
+//
 //! \fn print_Self_Test_Log()
 //
 //! \brief
@@ -176,6 +230,47 @@ void CScsi_DST_Results::print_Self_Test_Log(JSONNODE *dstNode, uint16_t run)
 {
 	std::string myStr = "";
 	myStr.resize(BASIC);
+#if defined (PREPYTHON)
+	json_push_back(dstNode, json_new_a("name", "self_test"));
+	JSONNODE* label = json_new(JSON_NODE);
+	json_set_name(label, "labels");
+	if (M_GETBITRANGE(m_DST->stCode, 7, 5) == DST_NOT_RUN)
+	{
+		json_push_back(label, json_new_a("stat_type", "self test not ran"));
+	}
+	else
+	{
+		get_PrePython_Self_Test_Results_String(myStr, M_GETBITRANGE(m_DST->stCode, 3, 0));
+		json_push_back(label, json_new_a("stat_type", (char*)myStr.c_str()));
+	}
+	snprintf((char*)myStr.c_str(), BASIC, "%" PRIu16"", run);
+	json_push_back(label, json_new_a("self_test_entry", (char*)myStr.c_str()));
+	snprintf((char*)myStr.c_str(), BASIC, "%02" PRIx8"", (uint8_t)M_GETBITRANGE(m_DST->stCode, 7, 5));
+	json_push_back(label, json_new_a("self_test_code", (char*)myStr.c_str()));
+	snprintf((char*)myStr.c_str(), BASIC, "%02" PRIx8"", (uint8_t)M_GETBITRANGE(m_DST->stCode, 3, 0));
+	json_push_back(label, json_new_a("self_test_results", (char*)myStr.c_str()));
+	snprintf((char*)myStr.c_str(), BASIC, "%" PRIu32"", m_DST->stNumber);
+	json_push_back(label, json_new_a("Self Test Number", (char*)myStr.c_str()));
+
+	snprintf((char*)myStr.c_str(), BASIC, "%" PRIu16"", m_DST->accPOH);
+	json_push_back(label, json_new_a("power_on_hours", (char*)myStr.c_str()));
+	snprintf((char*)myStr.c_str(), BASIC, "%" PRIx64"", m_DST->address);
+	json_push_back(label, json_new_a("first_failure", (char*)myStr.c_str()));
+
+	snprintf((char*)myStr.c_str(), BASIC, "%" PRIu8"", m_DST->senseKey);
+	json_push_back(label, json_new_a("scsi_sense_key", (char*)myStr.c_str()));
+	snprintf((char*)myStr.c_str(), BASIC, "%" PRIu8"", m_DST->addSenseCode);
+	json_push_back(label, json_new_a("scsi_asc", (char*)myStr.c_str()));
+	snprintf((char*)myStr.c_str(), BASIC, "%" PRIu8"", m_DST->addSenseCodeQualifier);
+	json_push_back(label, json_new_a("scsi_ascq", (char*)myStr.c_str()));
+	json_push_back(label, json_new_a("units", "failure"));
+
+	snprintf((char*)myStr.c_str(), BASIC, "scsi-log-page:0x%" PRIx8",%" PRIx8":0x%" PRIx16":%" PRIx8":%" PRIx8"", SELF_TEST_RESULTS, 0, m_DST->paramCode, m_DST->paramControlByte, m_DST->paramLength);
+	json_push_back(label, json_new_a("metric_source", (char*)myStr.c_str()));
+	json_push_back(dstNode, label);
+	json_push_back(dstNode, json_new_f("value", 0));    
+
+#else
 	JSONNODE *runInfo = json_new(JSON_NODE);
 	snprintf((char*)myStr.c_str(), BASIC, "Entry %3d ", run);   // changed the run# to Entry per Paul
 	json_set_name(runInfo, (char*)myStr.c_str());
@@ -212,6 +307,7 @@ void CScsi_DST_Results::print_Self_Test_Log(JSONNODE *dstNode, uint16_t run)
 	json_push_back(runInfo, json_new_a("Additional Sense Code Qualifier", (char*)myStr.c_str()));
 
 	json_push_back(dstNode, runInfo);
+#endif
 }
 //-----------------------------------------------------------------------------
 //
@@ -245,7 +341,7 @@ void CScsi_DST_Results::get_Self_Test_Results_String( std::string & meaning, uin
 		}
 		case DST_ABORTED:
 		{
-			meaning = "Was interepted by the host with a hard reset of a soft reset";
+			meaning = "Was interepted by the host with a hard reset or a soft reset";
 			break;
 		}
 		case DST_UNKNOWN_ERROR:
@@ -255,7 +351,7 @@ void CScsi_DST_Results::get_Self_Test_Results_String( std::string & meaning, uin
 		}
 		case DST_FAILURE_UNKNOWN_SEGMENT:
 		{
-			meaning = "Completed and has faild and the element is unknown";
+			meaning = "Completed and has failed and the element is unknown";
 			break;
 		}
 		case DST_FAILURE_FIRST_SEGMENT:
@@ -291,6 +387,86 @@ void CScsi_DST_Results::get_Self_Test_Results_String( std::string & meaning, uin
 			meaning = "Reserved.";
 			break;
 		}
+	}
+}
+//-----------------------------------------------------------------------------
+//
+//! \fn get_PrePython_Self_Test_Results_String()
+//
+//! \brief
+//!   Description: takes the data for each loop and produces the data for the json data.
+//
+//  Entry:
+//! \param meaning - string to add verbage to what happened from the testing 
+//! \param result - the result of the dst testing
+//
+//  Exit:
+//!   \return void
+//
+//---------------------------------------------------------------------------
+void CScsi_DST_Results::get_PrePython_Self_Test_Results_String(std::string& meaning, uint8_t result)
+{
+	meaning.resize(BASIC);
+	switch (result)
+	{
+	case DST_COMPLETED_WITHOUT_ERROR:
+	{
+		meaning = "completed without error";
+		break;
+	}
+	case DST_BACKGROUND_ABORTED:
+	{
+		meaning = "aborted by the host";
+		break;
+	}
+	case DST_ABORTED:
+	{
+		meaning = "interepted by host with a hard reset or a soft reset";
+		break;
+	}
+	case DST_UNKNOWN_ERROR:
+	{
+		meaning = "unknown error and self test unable to complete";
+		break;
+	}
+	case DST_FAILURE_UNKNOWN_SEGMENT:
+	{
+		meaning = "completed-failed and the element is unknown";
+		break;
+	}
+	case DST_FAILURE_FIRST_SEGMENT:
+	{
+		meaning = "completed-electrical element failing";
+		break;
+	}
+	case DST_FAILURE_SECOND_SEGMENT:
+	{
+		meaning = "completed-servo element failure";
+		break;
+	}
+	case DST_FAILURE_CHECK_NUMBER_FOR_SEGMENT:
+	{
+		meaning = "completed-read element failure";
+		break;
+	}
+	case DST_FAILURE_HANDLING_DAMAGE:
+	{
+		meaning = "completed-handling damage";
+	}
+	case DST_FAILURE_SUSPECTED_HANDLING_DAMAGE:
+	{
+		meaning = "completed-suspected handling damage";
+	}
+	case DST_IN_PROGRESS:
+	{
+		meaning = "self test-in progress";
+		break;
+	}
+	default:
+	{
+		meaning = "Reserved.";
+		break;
+	}
 	}
 }
 //-----------------------------------------------------------------------------
