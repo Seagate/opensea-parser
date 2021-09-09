cbfca61f0af6dc8bc713e99ae3eface84a513e36
Tim <Timothy.O.Gilmer@seagate.com>
2021-09-09 09:09:14 -0600

    LIB: changed to uint32_t for the printing of the offset

--- src/CScsi_Non_Medium_Error_Count_Log.cpp
+++ src/CScsi_Non_Medium_Error_Count_Log.cpp
@@ -184,7 +184,7 @@ eReturnValues CScsiNonMediumErrorCountLog::get_Non_Medium_Error_Count_Data(JSONN
 	{
 		JSONNODE *pageInfo = json_new(JSON_NODE);
 		json_set_name(pageInfo, "Non-Medium Error Count Log - 6h");
-		for (size_t offset = 0; offset < m_PageLength; )
+		for (uint32_t offset = 0; offset < m_PageLength; )
 		{
 			if (offset < m_bufferLength && offset < UINT16_MAX)
 			{
