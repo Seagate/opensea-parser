9ef7952bc332695aee48e77a6a5a1e4dc4ec9f2c
Tim <Timothy.O.Gilmer@seagate.com>
2024-03-26 17:05:06 -0600

    changed g_verbosity to enum class eVerbosity_open

--- src/Opensea_Parser_Helper.cpp
+++ src/Opensea_Parser_Helper.cpp
@@ -1,7 +1,7 @@
 // Opensea_Parser_Helper.cpp   Definition of SeaParser specific functions, structures
 // Do NOT modify or remove this copyright and license
 //
-// Copyright (c) 2014 - 2023 Seagate Technology LLC and/or its Affiliates
+// Copyright (c) 2014 - 2024 Seagate Technology LLC and/or its Affiliates
 //
 // This software is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -12,8 +12,7 @@
 // \file Opensea_Parser_Helper.cpp   Definition of SeaParser specific functions, structures
 #include "Opensea_Parser_Helper.h"
 
-eVerbosityLevels    g_verbosity = VERBOSITY_DEFAULT;
-eDataFormat         g_dataformat = JSON_DATA;
+eVerbosity_open     g_verbosity = eVerbosity_open::VERBOSITY_DEFAULT;
 bool                g_parseUnknown = true;
 bool                g_parseNULL = false;
 time_t              g_currentTime;
